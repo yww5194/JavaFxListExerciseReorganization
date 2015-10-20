91d5213a2b510b37700f255a0439e576b64ae423
yww5194 <yww5194@OZWDLND346-108.win.psu.edu>
2015-10-20 18:10:40 -0400

    Move FXMLDocumentController.javainto new fx.controller package

--- PopulateList/src/fx/controller/FXMLDocumentController.java
+++ PopulateList/src/fx/controller/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package populatelist;
+package fx.controller;
 
 import model.Person;
 import jpa.controller.PersonJpaController;
